38be149830d52e3ae8ff4973ded1b88811af9d27
Lilinamay <rc4333@nyu.edu>
2022-05-03 13:13:35 -0400

    Update ReadMe.cs

--- Endless_Date/Assets/ReadMe.cs
+++ Endless_Date/Assets/ReadMe.cs
@@ -9,10 +9,13 @@ using System.Collections.Generic;
 //          click on choices to choose the option
 //          click on phone icon to open your phone and look on phone chat when you see the phone icon on the botton left
 //          click on flowers in the scene when they are close up to ask May about the flowers
+//start game from "start scene" in the scene folder and you can save and load your story.
+//press ESC during the game to change camera invert setting and restart/save/quit game
 
 //Credit: use inky language to write dialogues
 //        free assets from unity asset store
 //        dialogue box sprite from Persona 4/5
 //        audio from freesound
 //        dog/cat pics download off internet
+
 
